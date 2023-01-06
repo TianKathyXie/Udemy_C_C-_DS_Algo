ce5a4490625d6cc95fc3ef23b833f394ad7c5669
Tian Xie <51527839+TianKathyXie@users.noreply.github.com>
2023-01-06 16:22:38 -0600

    Initial Commit

---
+++ Udemy_C_C++_DS_Algo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Udemy_C_C++_DS_Algo
+//
+//  Created by 谢天 on 1/6/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
